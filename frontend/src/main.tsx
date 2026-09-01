92fda38f1e0750c73fc74eb46656c6074ce80e05
rajpatil_2424 <rp0887856@gmail.com>
2026-09-01 23:15:11 +0530

    feature admin

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,6 +12,7 @@ export const restaurantService = "http://localhost:5001";
 export const utilsService = "http://localhost:5002";
 export const realtimeService = "http://localhost:5004";
 export const riderService = "http://localhost:5005";
+export const adminService = "http://localhost:5006";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
